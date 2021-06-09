89cdb87691ea556e290e36f64afbc9b0fd16fc8f
Ataberk ÖKLÜ <42235468+AtaberkOKLU@users.noreply.github.com>
2021-06-09 18:36:53 +0300

    Update VGA_Controller.v
    Update / Edit Parameters to enhance the hierarchy

--- QuartusProjectFiles/VGA_Controller.v
+++ QuartusProjectFiles/VGA_Controller.v
@@ -1,10 +1,34 @@
 module VGA_Controller
 #(
-	parameter R_WIDTH = 8,
-	parameter G_WIDTH = 8,
-	parameter B_WIDTH = 8,
-	parameter CNTR_WIDTH_V = 10,
-	parameter CNTR_WIDTH_H = 10
+	// Color Width = 8
+	// C5 Has 24-bit DAC
+	parameter R_WIDTH 		= 8,
+	parameter G_WIDTH 		= 8,
+	parameter B_WIDTH 		= 8,
+	
+	parameter CNTR_WIDTH_V 	= 10,			// Max CounterY Value = 2^CNTR_WIDTH_V
+	parameter CNTR_WIDTH_H 	= 10,			// Max CounterX Value = 2^CNTR_WIDTH_H
+	
+	// Timing Constants For 800x600@60Hz
+	// Horizontal Timing
+	parameter FRONT_PORCH_H = 40,
+	parameter BACK_PORCH_H 	= 88,
+	parameter SYNC_PULSE_H 	= 128,
+	parameter VISIBLE_H 		= 800,
+
+	// Vertical Timing
+	parameter FRONT_PORCH_V = 1,
+	parameter BACK_PORCH_V 	= 23,
+	parameter SYNC_PULSE_V 	= 4,
+	parameter VISIBLE_V 		= 600,
+	
+	// Product Image Properties
+	parameter PRDCT_PIC_WIDTH 	= 100,
+	parameter PRDCT_PIC_HEIGHT = 100,
+	parameter NUM_OF_PRDCT 		= 12,
+	
+	// ROM Block Memory Propertie
+	parameter ROM_ADDR_BUS_WIDTH = 17  	// Max Accessible Addresses 2^17 > (100x100x12):(3x8 bit)
 )
 (
     input CLOCK_50,
@@ -12,21 +36,18 @@ module VGA_Controller
     output reg [R_WIDTH-1:0] VGA_R,
 	 output reg [G_WIDTH-1:0] VGA_G,
 	 output reg [B_WIDTH-1:0] VGA_B,
-	 output VGA_CLK,
-    output VGA_HS,
-    output VGA_VS,
-	 output VGA_BLANK_N
+	 output wire VGA_CLK,
+    output wire VGA_HS,
+    output wire VGA_VS,
+	 output wire VGA_BLANK_N
 );
-	 localparam PRDCT_PIC_WIDTH = 100;
-	 localparam PRDCT_PIC_HEIGHT = 100;
-	 localparam NUM_OF_PRDCT = 12;
 
     wire inDisplayArea;
 	 wire VGA_CLK_W;
     wire [CNTR_WIDTH_H-1:0] CounterX;
 	 wire [CNTR_WIDTH_V-1:0] CounterY;
 	 wire [R_WIDTH+G_WIDTH+B_WIDTH-1:0] VGA_RGB_Bus;
-	 reg 	[18:0] targetPixelAddr = 0;
+	 reg 	[ROM_ADDR_BUS_WIDTH-1:0] targetPixelAddr = 0;
 	 
 	 VGA_PLL VGA_PLL_inst0(
 		.refclk(CLOCK_50),
@@ -35,14 +56,25 @@ module VGA_Controller
 		.locked()
 	 );
 
-    HVSync_Generator HVSync_Generator_inst0(
-      .VGA_CLK(VGA_CLK_W),
-		.RST_N(RESET),
-      .VGA_HS(VGA_HS),
-      .VGA_VS(VGA_VS),
-      .CounterX(CounterX),
-      .CounterY(CounterY),
-      .inDisplayArea(inDisplayArea)
+    HVSync_Generator#(
+			.CNTR_WIDTH_V(CNTR_WIDTH_V), 
+			.CNTR_WIDTH_H(CNTR_WIDTH_H),
+			.FRONT_PORCH_H(FRONT_PORCH_H),
+			.BACK_PORCH_H(BACK_PORCH_H),
+			.SYNC_PULSE_H(SYNC_PULSE_H),
+			.VISIBLE_H(VISIBLE_H),
+			.FRONT_PORCH_V(FRONT_PORCH_V),
+			.BACK_PORCH_V(BACK_PORCH_V),
+			.SYNC_PULSE_V(SYNC_PULSE_V),
+			.VISIBLE_V(VISIBLE_V)
+		) HVSync_Generator_inst0(
+			.VGA_CLK(VGA_CLK_W),
+			.RST_N(RESET),
+			.VGA_HS(VGA_HS),
+			.VGA_VS(VGA_VS),
+			.CounterX(CounterX),
+			.CounterY(CounterY),
+			.inDisplayArea(inDisplayArea)
     );
 	 
 	 ImageROM ImageROM_inst0(
@@ -58,7 +90,7 @@ module VGA_Controller
     begin
       if (inDisplayArea)
 			begin
-				 targetPixelAddr <= CounterY * 800 + CounterX;
+				 targetPixelAddr <= CounterY * VISIBLE_H + CounterX;				// Uses 1 DSP Block
 				 VGA_R[R_WIDTH-1:0] <= VGA_RGB_Bus[R_WIDTH-1:0];
 				 VGA_G[G_WIDTH-1:0] <= VGA_RGB_Bus[R_WIDTH+G_WIDTH-1:R_WIDTH];
 				 VGA_B[B_WIDTH-1:0] <= VGA_RGB_Bus[R_WIDTH+G_WIDTH+B_WIDTH-1:R_WIDTH+G_WIDTH];
