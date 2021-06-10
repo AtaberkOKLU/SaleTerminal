6efe53d70ebccf2e6a06e0c66f3c83d3918ab231
Ataberk ÖKLÜ <42235468+AtaberkOKLU@users.noreply.github.com>
2021-06-11 00:32:57 +0300

    ImageLocator is improved

--- QuartusProjectFiles/ImageLocator.v
+++ QuartusProjectFiles/ImageLocator.v
@@ -1,5 +1,5 @@
 /*
- *		Module		:	Image Locator Logic Block
+ *		Module		:	Image Locator Combinational Logic Block
  * 	Description	: 	Find the ImageID from Layout and CounterX/Y, and
  *							Calculate & provide ROM_Addr.
  *		Inputs		: 	CounterX		=> X Posiiton in the Screen
@@ -11,114 +11,78 @@
  *		Owner			: 	Ataberk ÖKLÜ
  */
 
-module ImageLocator(
-	input  wire[9:0] 	CounterX,
-	input  wire[9:0] 	CounterY,
+module ImageLocator
+#(
+	// Color Width = 8
+	// C5 Has 24-bit DAC
+	parameter R_WIDTH 		= 8,
+	parameter G_WIDTH 		= 8,
+	parameter B_WIDTH 		= 8,
 	
-	output reg[16:0]  ROM_Addr,
-	output reg 			isImage,
-	output reg [23:0] black_white
+	parameter CNTR_WIDTH_V 	= 10,			// Max CounterY Value = 2^CNTR_WIDTH_V
+	parameter CNTR_WIDTH_H 	= 10,			// Max CounterX Value = 2^CNTR_WIDTH_H	// ROM Block Memory Propertie
+	parameter ROM_ADDR_BUS_WIDTH = 17, 	// Max Accessible Addresses 2^17 > (100x100x12):(3x8 bit)
+	
+		// Product Image Properties
+	parameter PRDCT_PIC_WIDTH 	= 100,
+	parameter PRDCT_PIC_HEIGHT = 100
+)(
+	input  wire[CNTR_WIDTH_H-1:0] 	CounterX,
+	input  wire[CNTR_WIDTH_V-1:0] 	CounterY,
+	
+	output reg[ROM_ADDR_BUS_WIDTH-1:0]  ROM_Addr,
+	output wire 			isImage,
+	output reg[R_WIDTH+G_WIDTH+B_WIDTH-1:0] black_white
 );
 
-reg [3:0] ImageID = 0;
+wire [3:0] ImageID;
 
+
+wire w_x0 = (CounterX > 307) && (CounterX < 407);
+wire w_x1 = (CounterX > 435) && (CounterX < 535);
+wire w_x2 = (CounterX > 563) && (CounterX < 663);
+wire w_x3 = (CounterX > 691) && (CounterX < 791);
+
+wire w_y0 = (CounterY > 19) && (CounterY < 119);
+wire w_y1 = (CounterY > 147) && (CounterY < 247);
+wire w_y2 = (CounterY > 275) && (CounterY < 375);
+
+wire w_logo = (CounterX > 19) && (CounterX < 9) && (CounterY > 279) && (CounterY < 49);
+
+wire im_0  = w_x0 && w_y0;
+wire im_1  = w_x1 && w_y0;
+wire im_2  = w_x2 && w_y0;
+wire im_3  = w_x3 && w_y0;
+
+wire im_4  = w_x0 && w_y1;
+wire im_5  = w_x1 && w_y1;
+wire im_6  = w_x2 && w_y1;
+wire im_7  = w_x3 && w_y1;
+
+wire im_8  = w_x0 && w_y2;
+wire im_9  = w_x1 && w_y2;
+wire im_10 = w_x2 && w_y2;
+wire im_11 = w_x3 && w_y2;
+
+Encoder16x4 Encoder16x4_inst0(
+	.in({3'b0,w_logo,im_11,im_10,im_9,im_8,im_7,im_6,im_5,im_4,im_3,im_2,im_1,im_0}),
+	.out(ImageID),
+	.valid(isImage)
+);
+
+	
 always @ (*)
 	begin
-		if((CounterX > 307) && (CounterY > 19) && (CounterX < 407) && (CounterY < 119)) 		// Image[0,0]
+		if(isImage)
 			begin
-				isImage <= 1;
-				ImageID <= 0;
-				black_white	<= 24'b0;
-			end
-		else if((CounterX > 435) && (CounterY > 19) && (CounterX < 535) && (CounterY < 119))	// Image[0,0]
-			begin
-				isImage <= 1;
-				ImageID <= 1;
-				black_white	<= 24'b0;
-			end
-		else if((CounterX > 563) && (CounterY > 19) && (CounterX < 663) && (CounterY < 119))	// Image[0,2]
-			begin
-				isImage <= 1;
-				ImageID <= 2;
-				black_white	<= 24'b0;
-			end
-		else if((CounterX > 691) && (CounterY > 19) && (CounterX < 791) && (CounterY < 119)) // Image[0,3]
-			begin
-				isImage <= 1;
-				ImageID <= 3;
-				black_white	<= 24'b0;
-			end
-		else if((CounterX > 307) && (CounterY > 147) && (CounterX < 407) && (CounterY < 247))// Image[1,0]
-			begin
-				isImage <= 1;
-				ImageID <= 4;
-				black_white	<= 24'b0;
-			end
-		else if((CounterX > 435) && (CounterY > 147) && (CounterX < 535) && (CounterY < 247))// Image[1,1]
-			begin
-				isImage <= 1;
-				ImageID <= 5;
-				black_white	<= 24'b0;
-			end
-		else if((CounterX > 563) && (CounterY > 147) && (CounterX < 663) && (CounterY < 247))// Image[1,2]
-			begin
-				isImage <= 1;
-				ImageID <= 6;
-				black_white	<= 24'b0;
-			end
-		else if((CounterX > 691) && (CounterY > 147) && (CounterX < 791) && (CounterY < 247))// Image[1,3]
-			begin
-				isImage <= 1;
-				ImageID <= 7;
-				black_white	<= 24'b0;
-			end
-		else if((CounterX > 307) && (CounterY > 275) && (CounterX < 407) && (CounterY < 375))// Image[2,0]
-			begin
-				isImage <= 1;
-				ImageID <= 8;
-				black_white	<= 24'b0;
-			end
-		else if((CounterX > 435) && (CounterY > 275) && (CounterX < 535) && (CounterY < 375))// Image[2,1]
-			begin
-				isImage <= 1;
-				ImageID <= 9;
-				black_white	<= 24'b0;
-			end
-		else if((CounterX > 563) && (CounterY > 275) && (CounterX < 663) && (CounterY < 375))// Image[2,2]
-			begin
-				isImage <= 1;
-				ImageID <= 10;
-				black_white	<= 24'b0;
-			end
-		else if((CounterX > 691) && (CounterY > 275) && (CounterX < 791) && (CounterY < 375))// Image[2,3]
-			begin
-				isImage <= 1;
-				ImageID <= 11;
-				black_white	<= 24'b0;
-			end
-		else if((CounterX > 19) && (CounterY > 9) && (CounterX < 279) && (CounterY < 49))    // Logo
-			begin
-				isImage <= 1;
-				ImageID <= 12;
-				black_white	<= 24'b0;
+				ROM_Addr = ImageID*PRDCT_PIC_WIDTH*PRDCT_PIC_HEIGHT + (CounterX-(307+ImageID[1:0]>>7)) + PRDCT_PIC_WIDTH*(CounterY-(19+((ImageID>>2)<<7)));
+				black_white = 24'b0;
 			end
 		else
 			begin
-				isImage 		<= 0;
-				black_white	<= 24'b0;
-				ImageID 		<= 15;
+				ROM_Addr = 0;
+				black_white = 24'b0;
 			end
-			
 	end
-	
-	always @ (*)
-		begin
-			if(isImage)
-				begin
-					ROM_Addr <= ImageID*100*100 + (CounterX-(307+ImageID[1:0]>>7)) + 100*(CounterY-(19+((ImageID>>2)<<7)));
-				end
-			else
-					ROM_Addr <= 0;
-		end
 
 endmodule
