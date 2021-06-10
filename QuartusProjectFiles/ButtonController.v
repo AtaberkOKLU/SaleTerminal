95ceacfce52e5402848450e6bc48b6d404e749c6
Ataberk ÖKLÜ <42235468+AtaberkOKLU@users.noreply.github.com>
2021-06-10 23:47:38 +0300

    Update ButtonController.v
    SW9 -> SW0

--- QuartusProjectFiles/ButtonController.v
+++ QuartusProjectFiles/ButtonController.v
@@ -2,7 +2,7 @@
  *		Module		:	Button Controller Block
  * 	Description	: 	It manages the roles of limited number of buttons
  *							to handle both informative and command inputs
- *		Inputs		: 	SW		=> SW[9] from the board
+ *		Inputs		: 	SW		=> SW[0] from the board
  *						:	KEY 	=> KEY# from the board
  *
  *		Outputs		: 	CMD_Reg	=> Provides Current States of CMD Keys
@@ -13,13 +13,13 @@
  */
 
 module ButtonController(
-	input wire [9:9] SW,
+	input wire [0:0] SW,
 	input wire [3:0] KEY,
 	output reg [3:0] CMD_Reg,
 	output reg [3:0] KEY_Reg
 );
 
-wire SW_Wire = SW[9];
+wire SW_Wire = SW[0];
 
 initial
 begin
@@ -31,13 +31,13 @@ always @ (SW_Wire, KEY)
 	begin
 		if(SW_Wire)
 			begin		
-				KEY_Reg <= 0;
-				CMD_Reg <= KEY;
+				KEY_Reg = 0;
+				CMD_Reg = KEY;
 			end
 		else
 			begin
-				KEY_Reg <= KEY;
-				CMD_Reg <= 0;
+				KEY_Reg = KEY;
+				CMD_Reg = 0;
 			end
 	end
 endmodule
