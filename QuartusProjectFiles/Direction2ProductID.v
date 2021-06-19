5113e51379f5d645b69be0e97e3616b8c832c890
Ataberk ÖKLÜ <42235468+AtaberkOKLU@users.noreply.github.com>
2021-06-19 12:37:54 +0300

    Direction2ProductID / BasketEdit Support
    State Machine
     - IN: Dir_in
    Button Controller Interface
     - IN: SW2 Clean Out
    Basket Controller Interface
     - IN: # of Products
     - OUT: Basket List ID

--- QuartusProjectFiles/Direction2ProductID.v
+++ QuartusProjectFiles/Direction2ProductID.v
@@ -14,31 +14,54 @@ module Direction2ProductID(
 	input wire RESET_N,
 	input wire Enable,
 	input wire CLOCK,
+	input wire CleanSW2,
+	input wire [3:0] BasketProductNum,
 	input wire [1:0] Dir_in,
 	
-	output reg[3:0] ProductID
+	output wire [3:0] ProductID
 );
 
 // Dir_in	: (<- ^ v ->) (00 01 10 11) 
 
+reg[3:0] ImgID;
+reg[3:0] BasketID;
+
 initial
-	ProductID = 0; 	// Start from most-left-up product
+	begin
+		ImgID = 0; 	// Start from most-left-up product
+		BasketID = 0;
+	end
 	
 // Negative Edge Triggered Sequential Circuit
 always @ (negedge CLOCK)
 	if(!RESET_N)
-		ProductID = 0;
+		begin
+			ImgID = 0; 
+			BasketID = 0;
+		end
 	else
 		if(Enable)
-			begin
-				case(Dir_in)
-					2'b00	 : if(ProductID > 0 ) ProductID <= ProductID - 4'd1; else ProductID <= 4'b0000;
-					2'b01	 : if(ProductID > 3 ) ProductID <= ProductID - 4'd4; else ProductID <= 4'b0000;
-					2'b10	 : if(ProductID < 8 ) ProductID <= ProductID + 4'd4; else ProductID <= 4'd11;
-					2'b11	 : if(ProductID < 11) ProductID <= ProductID + 4'd1; else ProductID <= 4'd11;
-					default: ProductID <= 0;	// Never Reached
-				endcase
-			end
+			if(!CleanSW2)
+				begin
+					BasketID = 0;
+					case(Dir_in)
+						2'b00	 : if(ImgID > 0 ) ImgID <= ImgID - 4'd1; else ImgID <= 4'b0000;
+						2'b01	 : if(ImgID > 3 ) ImgID <= ImgID - 4'd4; else ImgID <= 4'b0000;
+						2'b10	 : if(ImgID < 8 ) ImgID <= ImgID + 4'd4; else ImgID <= 4'd11;
+						2'b11	 : if(ImgID < 11) ImgID <= ImgID + 4'd1; else ImgID <= 4'd11;
+						default: ImgID <= 0;	// Never Reached
+					endcase
+				end
+			else
+				begin
+					ImgID = 0;
+					case(Dir_in)
+						2'b01	 : if(BasketID > 0  ) BasketID <= BasketID - 4'd1; else BasketID <= 4'b0000;
+						2'b10	 : if(BasketID < BasketProductNum ) BasketID <= BasketID + 4'd1; else BasketID <= BasketProductNum;
+					endcase
+				end
+				
+assign ProductID = (CleanSW2) ? BasketID:ImgID;
 
 endmodule
 	
