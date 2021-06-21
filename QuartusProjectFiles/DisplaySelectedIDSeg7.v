f9fce35424cbc06de64310fc1ea1d6d1bcab29c5
Ataberk ÖKLÜ <42235468+AtaberkOKLU@users.noreply.github.com>
2021-06-21 22:46:05 +0300

    DisplaySelectedIDSeg7 is implemented to observe selected product

---
+++ QuartusProjectFiles/DisplaySelectedIDSeg7.v
@@ -0,0 +1,39 @@
+module DisplaySelectedIDSeg7(
+	input wire  [3:0] SelectedID,
+	input wire  [2:1] CleanSWOut,
+	input wire 			valid,
+	
+	output wire [6:0] HEX4,
+	output wire [6:0] HEX5
+
+);
+
+wire [7:0] BCD;
+
+Binary2BCD_2D Binary2BCD_2D_inst0(
+	.bin(SelectedID),
+	.bcd(BCD)
+);
+
+
+wire [3:0] FirstDigit;
+wire [3:0] SecndDigit;
+
+
+assign FirstDigit = (valid || |CleanSWOut) ? BCD[3:0] : 4'b1111;
+assign SecndDigit = (valid || |CleanSWOut) ? BCD[7:4] : 4'b1111;
+
+
+Segment7 Segment7_HEX4(
+	.bcd(FirstDigit),
+	.seg(HEX4)
+);
+
+Segment7 Segment7_HEX5(
+	.bcd(SecndDigit),
+	.seg(HEX5)
+);
+
+
+
+endmodule
