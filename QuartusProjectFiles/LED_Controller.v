c35d84bc56c578f7f23b26bd87e1f441d264aa66
Ataberk ÖKLÜ <42235468+AtaberkOKLU@users.noreply.github.com>
2021-06-21 22:42:48 +0300

    Update LED_Controller.v
    KEY and CMD inputs are assign to LEDs

--- QuartusProjectFiles/LED_Controller.v
+++ QuartusProjectFiles/LED_Controller.v
@@ -1,7 +1,8 @@
 module LED_Controller(
 	input  wire [2:0] State,
 	input  wire [2:0] CleanSWOut,
-	input  wire [3:0] CleanButtonOut,
+	input  wire [3:0] KEY_Reg,
+	input  wire [3:0] CMD_Reg,
 
 	output wire [9:0] LEDR
 );
@@ -9,6 +10,6 @@ module LED_Controller(
 assign LEDR[9:7] = State;
 assign LEDR[2:0] = CleanSWOut;
 
-assign LEDR[6:3] = CleanButtonOut;
+assign LEDR[6:3] = (CleanSWOut[0]) ? CMD_Reg : KEY_Reg;
 
 endmodule
