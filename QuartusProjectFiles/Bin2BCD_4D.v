6f03329d68aa001e8f8cf98fdd0373d68d9ee61e
Ataberk ÖKLÜ <42235468+AtaberkOKLU@users.noreply.github.com>
2021-06-25 13:45:12 +0300

    Minor Optimizations
    Co-Authored-By: Ahsen Topbaş <70740850+ahsentopbas@users.noreply.github.com>

--- QuartusProjectFiles/Bin2BCD_4D.v
+++ QuartusProjectFiles/Bin2BCD_4D.v
@@ -7,33 +7,29 @@
 module Bin2BCD_4D(
 	
 		input wire[11:0] Binary,
-		output wire [15:0] BCD		
+		output reg [15:0] BCD		
 );
 
-reg [15:0] TEMP;
 reg [3:0] i;
 
 always @(Binary) begin
-	if(Binary !== 12'hFFF)
+	if(~&Binary)
 		begin
-			TEMP = 0;
-				for (i = 0; i < 12; i = i+4'b1)
+			BCD = 0;
+				for (i = 4'b0; i < 12; i = i+4'd1)
 					begin
-						 TEMP = {TEMP[14:0],Binary[11-i]};
+						 BCD = {BCD[14:0],Binary[11-i]};
 								
-						 if(i < 11 && TEMP[3:0] > 4) 
-							  TEMP[3:0] = TEMP[3:0] + 4'd3;
-						 if(i < 11  && TEMP[7:4] > 4)
-							  TEMP[7:4] = TEMP[7:4] + 4'd3;
-						 if(i < 11 && TEMP[11:8] > 4)
-							  TEMP[11:8] = TEMP[11:8] + 4'd3;
-						 if(i < 11 && TEMP[15:12] > 4)
-							  TEMP[15:12] = TEMP[15:12] + 4'd3;
+						 if(i < 11 && BCD[3:0] > 4) 
+							  BCD[3:0] = BCD[3:0] + 4'd3;
+						 if(i < 11  && BCD[7:4] > 4)
+							  BCD[7:4] = BCD[7:4] + 4'd3;
+						 if(i < 11 && BCD[11:8] > 4)
+							  BCD[11:8] = BCD[11:8] + 4'd3;
+						 if(i < 11 && BCD[15:12] > 4)
+							  BCD[15:12] = BCD[15:12] + 4'd3;
 					end
 		end 
-	else TEMP = 16'hFFFF;
-end	
-
-assign BCD = TEMP;
-	
+	else BCD = 16'hFFFF;
+end
 endmodule 
--- QuartusProjectFiles/Bin2BCD_5D.v
+++ QuartusProjectFiles/Bin2BCD_5D.v
@@ -7,35 +7,32 @@
 module Bin2BCD_5D(
 	
 		input wire[15:0] Binary,
-		output wire [19:0] BCD		
+		output reg [19:0] BCD		
 );
 
-reg [19:0] TEMP;
 reg [4:0] i;
 
 always @(Binary) begin
-	if(Binary !== 16'hFFFF)
+	if(~&Binary)
 		begin
-			TEMP = 0;
-				for (i = 0; i < 16; i = i+4'b1)
+			BCD = 20'b0;
+				for (i = 5'b0; i < 16; i = i+5'b1)
 					begin
-						 TEMP = {TEMP[18:0],Binary[15-i]};
+						 BCD = {BCD[18:0],Binary[15-i]};
 								
-						 if(i < 15 && TEMP[3:0] > 4) 
-							  TEMP[3:0] = TEMP[3:0] + 4'd3;
-						 if(i < 15  && TEMP[7:4] > 4)
-							  TEMP[7:4] = TEMP[7:4] + 4'd3;
-						 if(i < 15 && TEMP[11:8] > 4)
-							  TEMP[11:8] = TEMP[11:8] + 4'd3;
-						 if(i < 15 && TEMP[15:12] > 4)
-							  TEMP[15:12] = TEMP[15:12] + 4'd3;
-						 if(i < 15 && TEMP[19:16] > 4)
-							  TEMP[19:16] = TEMP[19:16] + 4'd3;
+						 if(i < 15 && BCD[3:0] > 4) 
+							  BCD[3:0] = BCD[3:0] + 4'd3;
+						 if(i < 15  && BCD[7:4] > 4)
+							  BCD[7:4] = BCD[7:4] + 4'd3;
+						 if(i < 15 && BCD[11:8] > 4)
+							  BCD[11:8] = BCD[11:8] + 4'd3;
+						 if(i < 15 && BCD[15:12] > 4)
+							  BCD[15:12] = BCD[15:12] + 4'd3;
+						 if(i < 15 && BCD[19:16] > 4)
+							  BCD[19:16] = BCD[19:16] + 4'd3;
 					end
 		end 
-	else TEMP = 20'hFFFFF;
+	else BCD = 20'hFFFFF;
 end	
-
-assign BCD = TEMP;
 	
 endmodule 
